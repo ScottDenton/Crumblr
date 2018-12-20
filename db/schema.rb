57b5ab985d8021925d84ca5bf62a488d33d53a65
ScottDenton <scottyd_14@hotmail.com>
2018-12-19 16:15:19 -0800

    Add image posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_213327) do
+ActiveRecord::Schema.define(version: 2018_12_19_233705) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "kind"
   end
 
 end
