2a4b8f7efab4fab0782a655375f1af94655982f3
ScottDenton <scottyd_14@hotmail.com>
2018-12-20 10:41:38 -0800

    seeds file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 post1 = Post.new{title: "Brownies", kind: 'text'}
-post1.content = <<CONTENT
+post1.content = "
 Preheat oven to 350 degrees F (175 degrees C). Grease and flour an 8-inch square pan.
 
 In a large saucepan, melt 1/2 cup butter. Remove from heat, and stir in sugar, eggs, and 1 teaspoon vanilla. Beat in 1/3 cup cocoa, 1/2 cup flour, salt, and baking powder. Spread batter into prepared pan.
 
 Bake in preheated oven for 25 to 30 minutes. Do not overcook.
 
-To Make Frosting: Combine 3 tablespoons softened butter, 3 tablespoons cocoa, honey, 1 teaspoon vanilla extract, and 1 cup confectioners' sugar. Stir until smooth. Frost brownies while they are still warm.
-CONTENT
+To Make Frosting: Combine 3 tablespoons softened butter, 3 tablespoons cocoa, honey, 1 teaspoon vanilla extract, and 1 cup confectioners' sugar. Stir until smooth. Frost brownies while they are still warm."
+
 post1.save
 3.times do {post1.hearts.create}
 
